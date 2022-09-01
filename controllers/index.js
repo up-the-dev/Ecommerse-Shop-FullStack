66fc8945d0841ea5b58a29ad598e5eefc0d0b35c
UmeshPawar08 <umesh.a.pawar2002@gmail.com>
2022-09-01 11:00:25 +0530

    login controller added
    login controller added

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,6 @@
 module.exports = {
     admincontroller: require('./product/admin'),
     shopcontroller: require('./product/shop'),
-    registrationcontroller: require('./auth/register')
+    registrationcontroller: require('./auth/register'),
+    refreshController: require('../controllers/auth/refresh')
 }
